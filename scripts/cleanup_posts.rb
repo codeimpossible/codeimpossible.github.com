346d7fe62279204109ca7cd9e678b0ac7b1666ee
Jared Barboza <jbarboza@blackducksoftware.com>
2012-05-23 16:54:09 -0400

    moves posts around, still working on conversion

---
+++ scripts/cleanup_posts.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__) + '/reverse_markdown'
+
+Dir.glob(File.dirname(__FILE__) + '/export/*.{raw}') do |file|
+  contents = ""
+  reverser = ReverseMarkdown.new
+  File.open(file, "rb") do |opener|
+    contents = opener.read.gsub(/\t+/, '')
+  end
+
+  puts "Converting '#{file}'"
+
+  as_markdown = reverser.parse_string("<div>#{contents}</div>")
+
+  File.open("#{file.gsub(/\.raw/, '.md')}", 'w') do |writer|
+    as_markdown = as_markdown.gsub(/^slug:(.+)$/, "slug: '$1'")
+    writer.write as_markdown
+  end
+end
