102c4a7cab46bbf8d2fb0f93061794001127920c
Jared Barboza <jared.m.barboza@gmail.com>
2012-05-29 20:36:37 -0400

    fixes json accessors in javascript

--- assets/themes/the-minimum/js/site.js
+++ assets/themes/the-minimum/js/site.js
@@ -3,15 +3,16 @@
     var last_commit_url = "https://api.github.com/repos/codeimpossible/codeimpossible.github.com/commits?per_page=1&page=1";
     
     global.renderCommitSha = function(json) {
-      var sha = json.sha;
-      var commit_url = json.url;
-      var message = json.commit.message;
+      var commit = json.data[0];
+      var sha = commit.sha;
+      var commit_url = commit.url;
+      var message = commit.commit.message;
       var $html = $('<a />', { href: commit_url, text: sha, title: message, target: "_blank" });
       
       $html.appendTo('p.license');
     };
     
-    var html = "<scr" + "ipt src='" + last_commit_url + "?callback=renderCommitSha'></scr" + "ipt>"; 
+    var html = "<scr" + "ipt src='" + last_commit_url + "&callback=renderCommitSha'></scr" + "ipt>"; 
     
     $('body').append(html);
   });
