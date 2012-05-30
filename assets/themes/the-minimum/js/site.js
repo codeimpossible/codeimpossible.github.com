3554160fdeacc9e6c0aecb681749423d9d758e58
Jared Barboza <jared.m.barboza@gmail.com>
2012-05-29 20:43:40 -0400

    adds some logging to the script.

--- assets/themes/the-minimum/js/site.js
+++ assets/themes/the-minimum/js/site.js
@@ -3,7 +3,9 @@
     var last_commit_url = "https://api.github.com/repos/codeimpossible/codeimpossible.github.com/commits?per_page=1&page=1";
     
     global.renderCommitSha = function(json) {
+      console.log(json);
       var commit = json.data[0];
+      console.log(commit);
       var sha = commit.sha;
       var commit_url = commit.url;
       var message = commit.commit.message;
