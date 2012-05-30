81453441f51201156f4030ac34a682f64d084450
Jared Barboza <jared.m.barboza@gmail.com>
2012-05-29 20:46:10 -0400

    fixes selector
    the template maker made a spelling error...

--- assets/themes/the-minimum/js/site.js
+++ assets/themes/the-minimum/js/site.js
@@ -3,15 +3,13 @@
     var last_commit_url = "https://api.github.com/repos/codeimpossible/codeimpossible.github.com/commits?per_page=1&page=1";
     
     global.renderCommitSha = function(json) {
-      console.log(json);
       var commit = json.data[0];
-      console.log(commit);
       var sha = commit.sha;
       var commit_url = commit.url;
       var message = commit.commit.message;
       var $html = $('<a />', { href: commit_url, text: sha, title: message, target: "_blank" });
       
-      $html.appendTo('p.license');
+      $html.appendTo('p.licence');
     };
     
     var html = "<scr" + "ipt src='" + last_commit_url + "&callback=renderCommitSha'></scr" + "ipt>"; 
